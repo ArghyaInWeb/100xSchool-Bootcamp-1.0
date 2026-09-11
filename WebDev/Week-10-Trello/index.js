a41c3045ab0e664b39bc5eac20db8a3c12dcb443
ArghyaInWeb <ab.arghyabiswas2030@gmail.com>
2026-09-11 17:15:24 +0530

    Enhance organization management: add member invitation and removal features

--- WebDev/Week-10-Trello/index.js
+++ WebDev/Week-10-Trello/index.js
@@ -34,11 +34,14 @@ app.post("/signup", (req, res) => {
         })
     }
 
-    USERS.push({
-        id: USERS_ID++,
+    const newUser = {
+        id: USERS_ID,
         username,
         password,
-    })
+    }
+
+    USERS.push(newUser)
+    USERS_ID++
 
     res.json({
         message: "You are signed up successfully"
@@ -78,17 +81,20 @@ app.post("/onboard", (req, res) => {
 app.post("/organization", authenticate, (req, res) => {
     const userId = req.userId
 
-    ORGANIZATION.push({
+    const newOrg = {
         id: ORGANIZATION_ID,
         title: req.body.title,
         description: req.body.description,
         admin: userId,
         members: []
-    })
+    }
+
+    ORGANIZATION.push(newOrg)
+    ORGANIZATION_ID++
 
     res.json({
         message: "Organization created",
-        id: ORGANIZATION_ID - 1
+        id: newOrg.id //todo id or organization id on post/members
     })
 
 })
@@ -98,11 +104,81 @@ app.post("/create-board", (req, res) => {
 })
 
 // invite members
-app.post("/members", (req, res) => {
+app.post("/members", authenticate, (req, res) => {
+    const userId = req.userId
+    const organizationId = req.body.id //todo here!!!!
+    const memberUsername = req.body.userName
 
+    const organization = ORGANIZATION.find(org => org.id === organizationId)
+
+    if(!organization || organization.admin !== userId) {
+        res.status(411).json({
+            message: "Organization doesn't exist."
+        })
+        return
+    }
+
+    const invitedUser = USERS.find(u => u.username === memberUsername)
+
+    if(!invitedUser) {
+        res.status(403).json({
+            message: "User doesn't exist."
+        })
+        return
+    }
+
+    const alreadyMember = organization.members.some(id => id === invitedUser.id)
+    if(alreadyMember) {
+        res.status(409).json({
+            message: "User is already a member"
+        })
+        return
+    }
+    
+    organization.members.push(invitedUser.id)
+
+    res.json({
+        message: "Member successfully added to the organization"
+    })
 })
 
 //NOTE All GET endpoints
+
+// get all organizations
+app.get("/organization", authenticate, (req, res) => {
+    const userId = req.userId
+    const organizationId = Number(req.query.organizationId) //todo whether to add NUMBER
+
+    const organization = ORGANIZATION.find(org => org.id === organizationId)
+
+    if(!organization) {
+        res.status(411).json({
+            message: "Organization doesn't exist. 1"
+        })
+        return
+    }
+
+    if(organization.admin !== userId) {
+        res.status(411).json({
+            message: "Organization doesn't exist. 2"
+        })
+        return
+    }
+
+
+    res.json({
+        organization: {
+            ...organization,
+            members: organization.members.map(memberId => {
+                const user = USERS.find(u => u.id === memberId)
+                return {
+                    id: user.id,
+                    username: user.username
+                }
+            })
+        }
+    })
+})
 
 // get all boards
 app.get("/boards", (req, res) => {
@@ -124,7 +200,44 @@ app.put("issues", (req, res) => {
 
 })
 
+// delete/remove members
+app.delete("/members", authenticate, (req, res) => {
+    const userId = req.userId
+    const organizationId = req.body.id
+    const memberUsername = req.body.userName
 
+    const organization = ORGANIZATION.find(org => org.id === organizationId)
+
+    if(!organization || organization.admin !== userId) {
+        res.status(411).json({
+            message: "Organization doesn't exist."
+        })
+        return
+    }
+
+    const findUser = USERS.find(u => u.username === memberUsername)
+
+    if(!findUser) {
+        res.status(403).json({
+            message: "User doesn't exist."
+        })
+        return
+    }
+
+    const isMember = organization.members.includes(findUser.id)
+    if(!isMember) {
+        res.status(411).res.json({
+            message: "No user at this organization"
+        })
+        return
+    }
+
+    organization.members = organization.members.filter(id => id !== findUser.id)
+
+    res.json({
+        message: "Member removed from organization"
+    })
+})
 
 app.listen(3000, () => {
     console.log(`http://localhost:3000`)
