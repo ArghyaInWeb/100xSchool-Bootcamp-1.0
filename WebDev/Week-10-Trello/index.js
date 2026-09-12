1b737b342c9b1a905d66c06acb5477117c9018e3
ArghyaInWeb <ab.arghyabiswas2030@gmail.com>
2026-09-12 11:49:10 +0530

    Week 12 Migrating Trello to MongoDB - Updated the /signup &  /signin with mongoose operations

--- WebDev/Week-10-Trello/index.js
+++ WebDev/Week-10-Trello/index.js
@@ -1,21 +1,10 @@
 import express from "express"
 import jwt from "jsonwebtoken"
 import { authenticate } from "./auth.middleware.js"
+import { userModel, organizationModel, boardModel, issueModel } from "./models.js"
 
 const app = express()
 const JWT_SECRET = "trelloSecret"
-
-
-let USERS_ID = 1
-let ORGANIZATION_ID = 1
-let BOARD_ID = 1
-let ISSUES_ID = 1
-
-
-const USERS = []
-const ORGANIZATION = []
-const BOARD = []
-const ISSUES = []
 
 app.use(express.json())
 
@@ -23,45 +12,52 @@ app.use(express.json())
 //NOTE All POST endpoints
 
 // sign up
-app.post("/signup", (req, res) => {
+app.post("/signup", async(req, res) => {
     const username = req.body.username
     const password = req.body.password
 
-    const userExists = USERS.find(user => user.username === username)
+    const userExists = await userModel.findOne({
+        username: username,
+        password: password
+    })
+
     if(userExists) {
-        return res.status(403).json({
+        res.status(403).json({
             message: "User already exists"
         })
+        return
     }
 
-    const newUser = {
-        id: USERS_ID,
-        username,
-        password,
-    }
-
-    USERS.push(newUser)
-    USERS_ID++
+    const newUser = await userModel.create({
+        username: username,
+        password: password
+    })
 
     res.json({
+        userId: newUser._id,
         message: "You are signed up successfully"
     })
 })
 
 //sign in
-app.post("/signin", (req, res) => {
+app.post("/signin", async(req, res) => {
     const username = req.body.username
     const password = req.body.password
 
-    const userExists = USERS.find(user => user.username === username && user.password === password)
+    const userExists = await userModel.findOne({
+        username: username,
+        password: password
+    })
+
     if(!userExists) {
-        return res.status(403).json({
-            message: "User doesn't exists. Sign up first."
+        res.status(403).json({
+            message: "User does not exists"
         })
+        return
     }
 
     const token = jwt.sign({
-        userId: userExists.id
+        userId: userExists._id
     }, JWT_SECRET)
 
     res.json({
@@ -70,7 +66,6 @@ app.post("/signin", (req, res) => {
 
 })
 
-// app.use(authenticate())
 
 // onboarding
 app.post("/onboard", (req, res) => {
