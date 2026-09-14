be9131d7a1aa36c23dd6440bee0bce847cda6232
ArghyaInWeb <ab.arghyabiswas2030@gmail.com>
2026-09-14 17:41:01 +0530

    Week-14 Hashing Password - installed bcrypt and hashed password

--- WebDev/Week-13-1-SQL/index.js
+++ WebDev/Week-13-1-SQL/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { Pool } from "pg";
+import bcrypt from "bcrypt"
 
 // const pool = new Pool({
-//   connectionString:
+//   connectionString: 
 // });
 
 const app = express();
@@ -12,20 +13,15 @@ app.post("/signup", async (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
+  const hashedPassword = await bcrypt.hash(password, 10)
 
-//   const newUser = await pool.query(
-//     `INSERT INTO users (username, email, password) 
-//     VALUES ('${username}', '${email}', '${password}')
-//     RETURNING id`,
-//   );
 
     const newUser = await pool.query(`
         INSERT INTO users (username, email, password)
         VALUES ($1, $2, $3) RETURNING id`,
-        [username, email, password]
+        [username, email, hashedPassword]
     ) //! This syntax prevents SQL Injection 
 
-  console.log(newUser);
   res.json({
     message: "Sign up done",
     id: newUser.rows[0].id
@@ -33,20 +29,35 @@ app.post("/signup", async (req, res) => {
 });
 
 app.post("/signin", async (req, res) => {
-    const email = req.body.email
-    const password = req.body.password
+    const email = req.body.email;
+    const password = req.body.password;
 
     const isUser = await pool.query(
         `SELECT * FROM users
-        WHERE email=$1 AND password=$2`,
-        [email, password]
-    )
+        WHERE email=$1`,
+        [email]
+    );
 
-    console.log(isUser)
+    const userExists = isUser.rows[0]
+
+    if(!userExists) {
+      res.status(403).json({
+        message: "Incorrect credentials"
+      });
+      return;
+    };
+
+    const correctPassword = await bcrypt.compare(password, userExists.password)
+
+    if(!correctPassword) {
+      res.status(403).json({
+        message: "Incorrect password"
+      })
+    }
 
     res.json({
-        message: "Signed up successfully"
-    })
+        message: "Signed in successfully"
+    });
 });
 
 app.listen(3000);
