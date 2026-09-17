0413bc8978e080a82aec840a381b869e7d62474d
ArghyaInWeb <ab.arghyabiswas2030@gmail.com>
2026-09-17 18:26:57 +0530

    fix: Prisma client setup

--- Backend-Practice-Projects/Notes/src/lib/prisma.ts
+++ Backend-Practice-Projects/Notes/src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "../../generated/prisma/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-export const prisma = new PrismaClient({ adapter })
+export const client = new PrismaClient({ adapter })
